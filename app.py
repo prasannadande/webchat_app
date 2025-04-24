b9d4d05ba49de8f8d30877ea5f669a3ab1556313
prasannadande <prasanna.dande@gmail.com>
2025-04-24 12:15:00 -0400

    Fixed username injection into index.html

--- app.py
+++ app.py
@@ -1,15 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_socketio import SocketIO, send
+from flask_session import Session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+app.config['SESSION_TYPE'] = 'filesystem'
 
+Session(app)
+socketio = SocketIO(app, manage_session=False)
+
+users = {}  # username: password
 chat_history = []
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = ''
+    if request.method == 'POST':
+        uname = request.form['username']
+        pwd = request.form['password']
+        if uname in users and users[uname] == pwd:
+            session['username'] = uname
+            return redirect(url_for('home'))
+        error = 'Invalid credentials'
+    return render_template('login.html', error=error)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        uname = request.form['username']
+        pwd = request.form['password']
+        if uname not in users:
+            users[uname] = pwd
+            return redirect(url_for('login'))
+        else:
+            return "Username already exists!"
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 @app.route('/history')
 def history():
@@ -23,4 +59,4 @@ def handle_message(msg):
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
